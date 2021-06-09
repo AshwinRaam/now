6fc6942bfb7af70a502e1e2594e04a1df76f8347
dhanasekar5132 <dhanasekar5132@gmail.com>
2021-06-09 23:52:43 +0530

    added time cmd description

--- cmd/time.go
+++ cmd/time.go
@@ -29,9 +29,9 @@ import (
 // timeCmd represents the time command
 var timeCmd = &cobra.Command{
 	Use:   "time",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "time shows the time we spend on each task",
+	Long: `time shows how time time we spend on each task by subtracting the current time
+	with the task started time.
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
